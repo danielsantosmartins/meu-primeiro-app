2ce5cc6b337f1c1e177dcbe335a55195c6cf368c
Daniel Dos Santos Martins <danielsantosmartins.dev@gmail.com>
2023-07-22 15:29:23 -0300

    Desafio aula 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { TituloComponent } from './components/titulo/titulo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, TituloComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
